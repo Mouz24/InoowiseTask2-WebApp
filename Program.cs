79e39b0c5bcae132b1876263cadab279de49d2cc
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-10-16 18:11:52 +0300

    Done

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using IdentityServer4.Stores;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddIdentityServer()
+    .AddInMemoryCaching()
+    .AddClientStore<InMemoryClientStore>()
+    .AddResourceStore<InMemoryResourcesStore>();
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseSession();
+//app.Use(async (context, next) =>
+//{
+//    var token = context.Session.GetString("Token");
+//    if (!string.IsNullOrEmpty(token))
+//    {
+//        context.Request.Headers.Add("Authorization", "Bearer " + token);
+//    }
+//    await next();
+//});
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.Run();
